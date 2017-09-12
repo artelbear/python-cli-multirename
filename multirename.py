b888a1526b97e8cd661b28405de234987dfd715f
Nikolay <kn@localhost.localdomain>
2017-09-12 11:02:57 +0300

    version 1.0.0.1

--- multirename.py
+++ multirename.py
@@ -4,11 +4,21 @@ directory = input("Where:\t")
 if directory == "":
     directory = "."
 oldpattern = input("What:\t")
+if oldpattern == "":
+    oldpattern = "*"
 newpattern = input("To:\t")
 os.chdir(directory)
 filenames = os.listdir()
+i = 0
 for old in filenames:
-    if oldpattern in old:
+    if oldpattern == "*":
+        i = i + 1
+        new = newpattern + str(i)
+        print(old + "\t\tchange to\t\t" + new)
+        os.renames(old, new)
+    elif oldpattern in old:
+        i = i + 1
         new = old.replace(oldpattern, newpattern)
+        new = new + str(i)
         print(old + "\t\tchange to\t\t" + new)
         os.renames(old, new)
